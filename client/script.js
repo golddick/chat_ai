9a9e680813ef39bd731c03ab2d08671e801e6bb1
gee dick <golddick60@gmail.com>
2023-01-21 13:18:19 +0100

    chatai

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch(" http://localhost:9000", {
+    const response = await fetch("https://g-chat.onrender.com", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
