0ea01df13e66b69ebb0cf4752d73d7187b2edc88
gee dick <golddick60@gmail.com>
2023-01-21 11:57:45 +0100

    chat

--- client/script.js
+++ client/script.js
@@ -34,9 +34,6 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
