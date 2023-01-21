dc7098001bbe54f885ad99b7857ad7c56888802a
gee dick <golddick60@gmail.com>
2023-01-21 14:05:38 +0100

    client

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch("http://localhost:9000", {
+    const response = await fetch("https://g-chat-server.onrender.com", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
