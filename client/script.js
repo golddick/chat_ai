a155252938a44890a7e439a52c6a28dbb8149a92
gee dick <golddick60@gmail.com>
2023-01-21 12:47:43 +0100

    AI

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch("https://g-chat.onrender.com", {
+    const response = await fetch("", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
